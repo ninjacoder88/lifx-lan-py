b5e17bfcf90ef2a8443d18fd7bde97c0d181015e
ninjacoder88 <email@harrisconsulting.xyz>
2018-12-16 16:10:19 -0600

    moved hardcoded host and port as parameters

--- src/PacketSender.py
+++ src/PacketSender.py
@@ -1,12 +1,10 @@
 import socket
 
-def broadcastPacket(packetBytes):
-    HOST = "172.16.10.255"
-    PORT = 56700
+def broadcastPacket(host, port, packetBytes):
     
     with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as s:
         #s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
         s.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
-        s.sendto(packetBytes, (HOST, PORT))
+        s.sendto(packetBytes, (host, port))
         
     return
