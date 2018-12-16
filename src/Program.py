09f721d2244be2ba7c834d165905986365c269e1
ninjacoder88 <email@harrisconsulting.xyz>
2018-12-16 17:28:44 -0600

    added additional packet methods

--- src/Program.py
+++ src/Program.py
@@ -1,11 +1,26 @@
 import PacketBuilder
 import PacketSender
 
-def buildGetServicePacket():
-    return PacketBuilder.buildPacket(1, 1, 0, 0, 0, 0, 0, 2, "")
+#1227133513
 
-rawPacket = buildGetServicePacket()
-littleEndianPacket = PacketBuilder.convertRawPacketToLittleEndian(rawPacket)
-hexString = PacketBuilder.convertPacketToHexString(littleEndianPacket)
-packetBytes = bytes.fromhex(hexString)
-PacketSender.broadcastPacket(packetBytes)
+def buildGetServicePacket():
+    dictionary = {"tagged": 1, "source": 0, "target": 0, "ack": 0, "res": 0, "sequence": 0, "type": 2}
+    return PacketBuilder.buildPacket(dictionary, "")
+
+def buildSetPowerPacket():
+    dictionary = {"tagged": 0, "source": 0, "target": 8345188618731978752, "ack": 0, "res": 0, "sequence": 0, "type": 21} 
+    level = int((float(50) / 100) * 65535) 
+    payload = PacketBuilder.integerToBinary(level, 16)
+    return PacketBuilder.buildPacket(dictionary, payload)
+
+def buildGetLabelPacket():
+    dictionary = {"tagged": 0, "source": 0, "target": 0, "ack": 0, "res": 0, "sequence": 0, "type": 23}
+    return PacketBuilder.buildPacket(dictionary, "")
+
+HOST = "172.16.10.255"
+PORT = 56700
+
+#packetBytes = buildGetServicePacket()
+#packetBytes = buildSetPowerPacket()
+packetBytes = buildGetLabelPacket()
+PacketSender.broadcastPacket(HOST, PORT, packetBytes)
