49eabb4774078b15cf5aeb9c87f1f209a39e60fe
ninjacoder88 <email@harrisconsulting.xyz>
2018-12-16 18:09:30 -0600

    removed packet building methods for now

--- src/Program.py
+++ src/Program.py
@@ -7,20 +7,10 @@ def buildGetServicePacket():
     dictionary = {"tagged": 1, "source": 0, "target": 0, "ack": 0, "res": 0, "sequence": 0, "type": 2}
     return PacketBuilder.buildPacket(dictionary, "")
 
-def buildSetPowerPacket():
-    dictionary = {"tagged": 0, "source": 0, "target": 8345188618731978752, "ack": 0, "res": 0, "sequence": 0, "type": 21} 
-    level = int((float(50) / 100) * 65535) 
-    payload = PacketBuilder.integerToBinary(level, 16)
-    return PacketBuilder.buildPacket(dictionary, payload)
-
-def buildGetLabelPacket():
-    dictionary = {"tagged": 0, "source": 0, "target": 0, "ack": 0, "res": 0, "sequence": 0, "type": 23}
-    return PacketBuilder.buildPacket(dictionary, "")
-
 HOST = "172.16.10.255"
 PORT = 56700
 
 #packetBytes = buildGetServicePacket()
-#packetBytes = buildSetPowerPacket()
-packetBytes = buildGetLabelPacket()
+packetBytes = buildSetPowerPacket()
+#packetBytes = buildGetLabelPacket()
 PacketSender.broadcastPacket(HOST, PORT, packetBytes)
