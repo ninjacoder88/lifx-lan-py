ed14783418f9e381d6666142d5537e701a61fccd
ninjacoder88 <email@harrisconsulting.xyz>
2018-12-16 18:33:03 -0600

    renamed Program to Broadcast

--- src/Broadcast.py
+++ src/Broadcast.py
@@ -4,8 +4,8 @@ import PacketSender
 #1227133513
 
 def buildGetServicePacket():
-    dictionary = {"tagged": 1, "source": 0, "target": 0, "ack": 0, "res": 0, "sequence": 0, "type": 2}
-    return PacketBuilder.buildPacket(dictionary, "")
+    fieldDictionary = {"tagged": 1, "source": 0, "target": 0, "ack": 0, "res": 0, "sequence": 0, "type": 2}
+    return PacketBuilder.buildPacket(fieldDictionary, "")
 
 HOST = "172.16.10.255"
 PORT = 56700
