a36005bbaa1a9dd8ad8de7a89325f3f465951278
ninjacoder88 <email@harrisconsulting.xyz>
2018-12-18 17:04:36 -0600

    renamed Listen to PacketReceiver

--- src/PacketReceiver.py
+++ src/PacketReceiver.py
@@ -1,18 +1,14 @@
 import socket
 import PacketProcessor
 
-def listenForPackets():
-    HOST = ''
-    PORT = 56700
+def receivePackets(host, port):
 
     with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as s:
         s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-        s.bind((HOST, PORT))
+        s.bind((host, port))
         data_buffer = []
         while True:
             data = s.recvfrom(512)[0]
             PacketProcessor.processData(data)
             
     return
-
-listenForPackets()
