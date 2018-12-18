cfff93e18521a7280e4bef73019c88cd31f63544
ninjacoder88 <email@harrisconsulting.xyz>
2018-12-18 17:16:05 -0600

    renamed Broadcast to PacketBuilder

--- src/PacketBuilder.py
+++ src/PacketBuilder.py
@@ -1,8 +1,6 @@
 import PacketBuilder
 import PacketSender
 
-#1227133513
-
 def buildDeviceEchoRequestPacket(payload):
     fieldDictionary = {"tagged": 1, "source": 0, "target": 0, "ack": 0, "res": 0, "sequence": 0, "type": 58}
     return PacketBuilder.buildPacket(fieldDictionary, payload)
@@ -11,7 +9,11 @@ def buildDeviceGetServicePacket():
     fieldDictionary = {"tagged": 1, "source": 0, "target": 0, "ack": 0, "res": 0, "sequence": 0, "type": 2}
     return PacketBuilder.buildPacket(fieldDictionary, "")
 
-def buildGetPacket(type):
+def buildDeviceGetPacket(type):
+    fieldDictionary = {"tagged": 0, "source": 0, "target": 0, "ack": 0, "res": 0, "sequence": 0, "type": type}
+    return PacketBuilder.buildPacket(fieldDictionary, "")
+
+def buildLightGetPacket(type):
     fieldDictionary = {"tagged": 0, "source": 0, "target": 0, "ack": 0, "res": 0, "sequence": 0, "type": type}
     return PacketBuilder.buildPacket(fieldDictionary, "")
 
@@ -83,7 +85,7 @@ def buildLightSetInfraredPacket(brightnessValue):
     return PacketBuilder.buildPacket(fieldDictionary, payload)
     
 
-#packetBytes = buildDeviceGetServicePacket() #DEVICE_GetService
+#packetBytes = buildDeviceGetServicePacket() #DEVICE_GetService #1227133513
 #packetBytes = buildDeviceEchoRequestPacket("00110011001100110011") #DEVICE_EchoRequest (payload byte array; 64 bytes)
 #packetBytes = buildGetPacket(12) #DEVICE_GetHostInfo
 #packetBytes = buildGetPacket(14) #DEVICE_GetHostFirmware
@@ -97,8 +99,3 @@ def buildLightSetInfraredPacket(brightnessValue):
 #packetBytes = buildGetPacket(101) #LIGHT_GetState
 #packetBytes = buildGetPacket(116) #LIGHT_GetPower
 #packetBytes = buildGetPacket(120) #LIGHT_GetInfrared
-
-HOST = "172.16.10.255"
-PORT = 56700
-
-PacketSender.broadcastPacket(HOST, PORT, packetBytes)
