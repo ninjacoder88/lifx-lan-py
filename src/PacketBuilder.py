a9a4989191daf27ab7c30bc25e34ae2a0df41c96
ninjacoder88 <email@harrisconsulting.xyz>
2018-12-18 18:36:49 -0600

    updated to reference PacketBuilderBase

--- src/PacketBuilder.py
+++ src/PacketBuilder.py
@@ -1,34 +1,25 @@
-import PacketBuilder
-import PacketSender
-
-def buildDeviceEchoRequestPacket(payload):
-    fieldDictionary = {"tagged": 1, "source": 0, "target": 0, "ack": 0, "res": 0, "sequence": 0, "type": 58}
-    return PacketBuilder.buildPacket(fieldDictionary, payload)
-
-def buildDeviceGetServicePacket():
-    fieldDictionary = {"tagged": 1, "source": 0, "target": 0, "ack": 0, "res": 0, "sequence": 0, "type": 2}
-    return PacketBuilder.buildPacket(fieldDictionary, "")
+import PacketBuilderBase
 
 def buildDeviceGetPacket(type):
     fieldDictionary = {"tagged": 0, "source": 0, "target": 0, "ack": 0, "res": 0, "sequence": 0, "type": type}
-    return PacketBuilder.buildPacket(fieldDictionary, "")
+    return PacketBuilderBase.buildPacket(fieldDictionary, "")
 
-def buildLightGetPacket(type):
-    fieldDictionary = {"tagged": 0, "source": 0, "target": 0, "ack": 0, "res": 0, "sequence": 0, "type": type}
-    return PacketBuilder.buildPacket(fieldDictionary, "")
+def buildDeviceGetServicePacket():
+    fieldDictionary = {"tagged": 1, "source": 0, "target": 0, "ack": 0, "res": 0, "sequence": 0, "type": 2}
+    return PacketBuilderBase.buildPacket(fieldDictionary, "")
 
 def buildDeviceSetPowerPacket(levelValue):
     #validate between 0 and 100
     fieldDictionary = {"tagged": 0, "source": 0, "target": 0, "ack": 0, "res": 0, "sequence": 0, "type": 21}
-    level = int(float(levelValue) / 100) * 65535)
+    level = int(float(levelValue) / 100) * 65535
     payload = "" # need to build payload
-    return PacketBuilder.buildPacket(fieldDictionary, payload)
+    return PacketBuilderBase.buildPacket(fieldDictionary, payload)
 
 def buildDeviceSetLabelPacket(labelValue):
     fieldDictionary = {"tagged": 0, "source": 0, "target": 0, "ack": 0, "res": 0, "sequence": 0, "type": 24}
     label = labelValue #32 byte string
     payload = "" # need to build payload
-    return PacketBuilder.buildPacket(fieldDictionary, payload)
+    return PacketBuilderBase.buildPacket(fieldDictionary, payload)
 
 def buildDeviceSetLocationPacket(locationValue, labelValue, updatedAtValue):
     fieldDictionary = {"tagged": 0, "source": 0, "target": 0, "ack": 0, "res": 0, "sequence": 0, "type": 49}
@@ -36,7 +27,7 @@ def buildDeviceSetLocationPacket(locationValue, labelValue, updatedAtValue):
     label = labelValue #32 byte string
     updatedAt = updatedAtValue #64 bit int
     payload = "" # need to build payload
-    return PacketBuilder.buildPacket(fieldDictionary, payload)
+    return PacketBuilderBase.buildPacket(fieldDictionary, payload)
 
 def buildDeviceSetGroupPacket(groupValue, labelValue, updatedAtValue):
     fieldDictionary = {"tagged": 0, "source": 0, "target": 0, "ack": 0, "res": 0, "sequence": 0, "type": 52}
@@ -44,7 +35,15 @@ def buildDeviceSetGroupPacket(groupValue, labelValue, updatedAtValue):
     label = labelValue #32 byte string
     updatedAt = updatedAtValue #64 bit int
     payload = "" # need to build payload
-    return PacketBuilder.buildPacket(fieldDictionary, payload)
+    return PacketBuilderBase.buildPacket(fieldDictionary, payload)
+
+def buildDeviceEchoRequestPacket(payload):
+    fieldDictionary = {"tagged": 1, "source": 0, "target": 0, "ack": 0, "res": 0, "sequence": 0, "type": 58}
+    return PacketBuilderBase.buildPacket(fieldDictionary, payload)
+
+def buildLightGetPacket(type):
+    fieldDictionary = {"tagged": 0, "source": 0, "target": 0, "ack": 0, "res": 0, "sequence": 0, "type": type}
+    return PacketBuilderBase.buildPacket(fieldDictionary, "")
 
 def buildLightSetColorPacket(colorValue, durationValue):
     fieldDictionary = {"tagged": 0, "source": 0, "target": 0, "ack": 0, "res": 0, "sequence": 0, "type": 102}
@@ -52,7 +51,7 @@ def buildLightSetColorPacket(colorValue, durationValue):
     color = colorValue #HSBK
     duration = durationValue #u32-bit int
     payload = "" # need to build payload
-    return PacketBuilder.buildPacket(fieldDictionary, payload)
+    return PacketBuilderBase.buildPacket(fieldDictionary, payload)
 
 def buildLightSetWaveformPacket(transientValue, colorValue, periodValue, cyclesValue, skewRationValue, waveformValue):
     fieldDictionary = {"tagged": 0, "source": 0, "target": 0, "ack": 0, "res": 0, "sequence": 0, "type": 103}
@@ -64,38 +63,22 @@ def buildLightSetWaveformPacket(transientValue, colorValue, periodValue, cyclesV
     skewRatio = skewRatioValue
     waveform = waveformValue
     payload = "" # need to build payload
-    return PacketBuilder.buildPacket(fieldDictionary, payload)
+    return PacketBuilderBase.buildPacket(fieldDictionary, payload)
     
 def buildLightSetWaveformOptionPacket():
     fieldDictionary = {"tagged": 0, "source": 0, "target": 0, "ack": 0, "res": 0, "sequence": 0, "type": 119}
     payload = "" # need to build payload
-    return PacketBuilder.buildPacket(fieldDictionary, payload)
+    return PacketBuilderBase.buildPacket(fieldDictionary, payload)
 
 def buildLightSetPowerPacket(levelValue, durationValue):
     fieldDictionary = {"tagged": 0, "source": 0, "target": 0, "ack": 0, "res": 0, "sequence": 0, "type": 117}
     level = levelValue
     duration = durationValue
     payload = "" # need to build payload
-    return PacketBuilder.buildPacket(fieldDictionary, payload)
+    return PacketBuilderBase.buildPacket(fieldDictionary, payload)
 
 def buildLightSetInfraredPacket(brightnessValue):
     fieldDictionary = {"tagged": 0, "source": 0, "target": 0, "ack": 0, "res": 0, "sequence": 0, "type": 122}
     brightness = brightnessValue
     payload = "" # need to build payload
-    return PacketBuilder.buildPacket(fieldDictionary, payload)
-    
-
-#packetBytes = buildDeviceGetServicePacket() #DEVICE_GetService #1227133513
-#packetBytes = buildDeviceEchoRequestPacket("00110011001100110011") #DEVICE_EchoRequest (payload byte array; 64 bytes)
-#packetBytes = buildGetPacket(12) #DEVICE_GetHostInfo
-#packetBytes = buildGetPacket(14) #DEVICE_GetHostFirmware
-#packetBytes = buildGetPacket(16) #DEVICE_GetWifiInfo
-#packetBytes = buildGetPacket(20) #DEVICE_GetPower
-#packetBytes = buildGetPacket(23) #DEVICE_GetLabel
-#packetBytes = buildGetPacket(32) #DEVICE_GetVersion
-#packetBytes = buildGetPacket(34) #DEVICE_GetInfo
-#packetBytes = buildGetPacket(48) #DEVICE_GetLocation
-#packetBytes = buildGetPacket(51) #DEVICE_GetGroup
-#packetBytes = buildGetPacket(101) #LIGHT_GetState
-#packetBytes = buildGetPacket(116) #LIGHT_GetPower
-#packetBytes = buildGetPacket(120) #LIGHT_GetInfrared
+    return PacketBuilderBase.buildPacket(fieldDictionary, payload)
