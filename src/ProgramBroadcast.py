4ea20939434843e60a0c505d6117cbe9effe0099
ninjacoder88 <email@harrisconsulting.xyz>
2018-12-18 20:02:11 -0600

    added PacketBuilder reference to method calls

--- src/ProgramBroadcast.py
+++ src/ProgramBroadcast.py
@@ -2,21 +2,25 @@ import PacketBroadcaster
 import PacketBuilder
 
 def broadcast():
-    #packetBytes = buildDeviceGetServicePacket() #DEVICE_GetService #1227133513
-    #packetBytes = buildDeviceEchoRequestPacket("00110011001100110011") #DEVICE_EchoRequest (payload byte array; 64 bytes)
-    #packetBytes = buildGetPacket(12) #DEVICE_GetHostInfo
-    #packetBytes = buildGetPacket(14) #DEVICE_GetHostFirmware
-    #packetBytes = buildGetPacket(16) #DEVICE_GetWifiInfo
-    #packetBytes = buildGetPacket(20) #DEVICE_GetPower
-    #packetBytes = buildGetPacket(23) #DEVICE_GetLabel
-    #packetBytes = buildGetPacket(32) #DEVICE_GetVersion
-    #packetBytes = buildGetPacket(34) #DEVICE_GetInfo
-    #packetBytes = buildGetPacket(48) #DEVICE_GetLocation
-    #packetBytes = buildGetPacket(51) #DEVICE_GetGroup
-    #packetBytes = buildGetPacket(101) #LIGHT_GetState
-    #packetBytes = buildGetPacket(116) #LIGHT_GetPower
-    #packetBytes = buildGetPacket(120) #LIGHT_GetInfrared
-    packetBytes = PacketBuilder.buildDeviceGetServicePacket()
+    
+    #packetBytes = PacketBuilder.buildDeviceEchoRequestPacket("00110011001100110011") #DEVICE_EchoRequest (payload byte array; 64 bytes)
+    #packetBytes = PacketBuilder.buildDeviceGetPacket(12) #DEVICE_GetHostInfo
+    #packetBytes = PacketBuilder.buildDeviceGetPacket(14) #DEVICE_GetHostFirmware
+    #packetBytes = PacketBuilder.buildDeviceGetPacket(16) #DEVICE_GetWifiInfo
+    #packetBytes = PacketBuilder.buildDeviceGetPacket(20) #DEVICE_GetPower
+    #packetBytes = PacketBuilder.buildDeviceGetPacket(23) #DEVICE_GetLabel
+    #packetBytes = PacketBuilder.buildDeviceGetPacket(32) #DEVICE_GetVersion
+    #packetBytes = PacketBuilder.buildDeviceGetPacket(34) #DEVICE_GetInfo
+    #packetBytes = PacketBuilder.buildDeviceGetPacket(48) #DEVICE_GetLocation
+    #packetBytes = PacketBuilder.buildDeviceGetPacket(51) #DEVICE_GetGroup
+    #packetBytes = PacketBuilder.buildLightGetPacket(101) #LIGHT_GetState
+    #packetBytes = PacketBuilder.buildLightGetPacket(116) #LIGHT_GetPower
+    #packetBytes = PacketBuilder.buildLightGetPacket(120) #LIGHT_GetInfrared
+    #packetBytes = PacketBuilder.buildDeviceGetServicePacket()
+    
+    #packetBytes = PacketBuilder.buildDeviceSetPowerPacket(0, 50)
+    packetBytes = PacketBuilder.buildDeviceGetServicePacket() #DEVICE_GetService #1227133513
+    
     PacketBroadcaster.broadcastPacket("172.16.10.255", 56700, packetBytes)
     return
 
