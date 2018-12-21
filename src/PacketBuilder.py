ab6199b00ba796f2258dd34257cfca77bbdb738d
ninjacoder88 <email@harrisconsulting.xyz>
2018-12-21 11:23:37 -0600

    added helper methods for convertering types to binary; add construction logic to for most packets

--- src/PacketBuilder.py
+++ src/PacketBuilder.py
@@ -1,11 +1,28 @@
 import PacketBuilderBase
 import EndianConverter
 
+def int_to_bin(integer_value, bits_to_occupy):
+    binary_string = EndianConverter.convert(bin(integer_value).replace("0b","").zfill(bits_to_occupy))
+    return binary_string
+
+def string_to_bin(string_value, bits_to_occupy):
+    binary_string = ""
+    return binary_string
+
+def float_to_bin(float_value, bits_to_occupy):
+    binary_string = ""
+    return binary_string
+
+def time_to_bin(time_value, bits_to_occupy):
+    binary_string = ""
+    return binary_string
+
+
 def build_device_get_packet(type):
     field_dictionary = {"tagged": 0, "source": 0, "target": 0, "ack": 0, "res": 0, "sequence": 0, "type": type}
     return PacketBuilderBase.buildPacket(field_dictionary, "")
 
-def build_device_get_service_packet():#validated
+def build_device_get_service_packet():
     field_dictionary = {"tagged": 1, "source": 0, "target": 0, "ack": 0, "res": 0, "sequence": 0, "type": 2}
     return PacketBuilderBase.buildPacket(field_dictionary, "")
 
@@ -27,17 +44,17 @@ def build_device_get_power_packet():
 def build_device_set_power_packet(source, level_value):
     #validate 0 or 100
     field_dictionary = {"tagged": 0, "source": source, "target": 0, "ack": 0, "res": 0, "sequence": 0, "type": 21}
-    level = int(float(level_value) / 100 * 65535)
-    payload = bin(level).replace("0b","").zfill(16)
+    level = int_to_bin(int(float(level_value) / 100 * 65535), 16)#unsigned 16 bit integer (0 or 65535)
+    payload = level
     return PacketBuilderBase.buildPacket(field_dictionary, payload)
 
 def build_device_get_label_packet():
     return build_device_get_packet(23)
 
-def build_device_set_label_packet(label_value):
-    field_dictionary = {"tagged": 0, "source": 0, "target": 0, "ack": 0, "res": 0, "sequence": 0, "type": 24}
-    label = label_value #32 byte string
-    payload = "" # need to build payload
+def build_device_set_label_packet(source, label_value):
+    field_dictionary = {"tagged": 0, "source": source, "target": 0, "ack": 0, "res": 0, "sequence": 0, "type": 24}
+    label = string_to_bin(label_value, 256)#32 byte string
+    payload = label
     return PacketBuilderBase.buildPacket(field_dictionary, payload)
 
 def build_device_get_version_packet():
@@ -49,23 +66,23 @@ def build_device_get_info_packet():
 def build_device_get_location_packet():
     return build_device_get_packet(48)
 
-def build_device_set_location_packet(location_value, label_value, updated_at_value):
-    field_dictionary = {"tagged": 0, "source": 0, "target": 0, "ack": 0, "res": 0, "sequence": 0, "type": 49}
-    location = location_value #byte array 16
-    label = label_value #32 byte string
-    updatedAt = updated_at_value #64 bit int
-    payload = "" # need to build payload
+def build_device_set_location_packet(source, location_value, label_value, updated_at_value):
+    field_dictionary = {"tagged": 0, "source": source, "target": 0, "ack": 0, "res": 0, "sequence": 0, "type": 49}
+    location = location_value #byte array of 16 bytes
+    label = string_to_bin(label_value, 256)#32 byte string
+    updated_at = time_to_bin(updated_at_value, 64)#unsigned 64 bit integer
+    payload = location + label + updated_at
     return PacketBuilderBase.buildPacket(field_dictionary, payload)
 
 def build_device_get_group_packet():
     return build_device_get_packet(51)
 
-def build_device_set_group_packet(group_value, label_value, update_at_value):
-    field_dictionary = {"tagged": 0, "source": 0, "target": 0, "ack": 0, "res": 0, "sequence": 0, "type": 52}
-    group = group_value #byte array 16
-    label = label_value #32 byte string
-    updatedAt = update_at_value #64 bit int
-    payload = "" # need to build payload
+def build_device_set_group_packet(source, group_value, label_value, update_at_value):
+    field_dictionary = {"tagged": 0, "source": source, "target": 0, "ack": 0, "res": 0, "sequence": 0, "type": 52}
+    group = group_value #byte array of 16 bytes
+    label = string_to_bin(label_value, 256)#32 byte string
+    updated_at = time_to_bin(update_at_value, 64)#unsigned 64 bit integer
+    payload = group + label + updated_at
     return PacketBuilderBase.buildPacket(field_dictionary, payload)
 
 def build_device_echo_request_packet(payload):
@@ -79,39 +96,59 @@ def build_light_get_packet(type):
 def build_light_get_state_packet():
     return build_light_get_packet(101)
 
-def build_light_set_color_packet(color_value, duration_value):
+def build_light_set_color_packet(hue_value, sat_value, brightness_value, kelvin_value, duration_value):
     field_dictionary = {"tagged": 0, "source": 0, "target": 0, "ack": 0, "res": 0, "sequence": 0, "type": 102}
-    reserved = "00000000" #u8bit-int
-    color = color_value #HSBK
-    duration = duration_value #u32-bit int
-    payload = "" # need to build payload
+    reserved = "".zfill(8) #unsigned 8 bit integer 
+    hue = int_to_bin(hue_value, 16)#unsigned 16 bit integer (0-65535)
+    saturation = int_to_bin(sat_value, 16)#unsigned 16 bit integer (0-65535)
+    brightness = int_to_bin(brightness_value, 16)#unsigned 16 bit integer (0-65535)
+    kelvin = int_to_bin(kelvin_value, 16)#unsigned 16 bit integer (2500-9000)
+    duration = int_to_bin(duration_value, 32)#unsigned 32 bit integer (transition in milliseconds)
+    payload = reserved + hue + saturation + brightness + kelving + duration
     return PacketBuilderBase.buildPacket(field_dictionary, payload)
 
-def build_ligth_set_waveform_packet(transient_value, color_value, period_value, cycles_value, skew_ration_value, waveform_value):
-    field_dictionary = {"tagged": 0, "source": 0, "target": 0, "ack": 0, "res": 0, "sequence": 0, "type": 103}
-    reserved = "00000000" #u8bit-int
-    transient = transient_value
-    color = color_value
-    period = period_value
-    cycles = cycles_value
-    skewRatio = skew_ration_value
-    waveform = waveform_value
-    payload = "" # need to build payload
+def build_ligth_set_waveform_packet(source, transient_value, hue_value, sat_value, brightness_value, kelvin_value, period_value, cycles_value, skew_ration_value, waveform_value):
+    field_dictionary = {"tagged": 0, "source": source, "target": 0, "ack": 0, "res": 0, "sequence": 0, "type": 103}
+    reserved = "".zfill(8)#unsigned 8 bit integer
+    transient = transient_value#8 bit integer (0 or 1)
+    hue = int_to_bin(hue_value, 16)#unsigned 16 bit integer (0-65535)
+    saturation = int_to_bin(sat_value, 16)#unsigned 16 bit integer (0-65535)
+    brightness = int_to_bin(brightness_value, 16)#unsigned 16 bit integer (0-65535)
+    kelvin = int_to_bin(kelvin_value, 16)#unsigned 16 bit integer (2500-9000)
+    period = period_value#unsigned 32 bit integer (cycle duration in milliseconds)
+    cycles = cycles_value#32 bit float (number of cycles)
+    skew_ratio = skew_ration_value#16 bit integer
+    waveform = waveform_value#unsinged 8 bit integer
+    payload = reserved + transient + hue + saturation + brightness + kelvin + period + cycles + skew_ratio + waveform
     return PacketBuilderBase.buildPacket(field_dictionary, payload)
     
 def build_light_get_power_packet():
     return build_light_get_packet(116)
 
-def build_light_set_power_packet(level_value, duration_value):
-    field_dictionary = {"tagged": 0, "source": 0, "target": 0, "ack": 0, "res": 0, "sequence": 0, "type": 117}
-    level = level_value
-    duration = duration_value
-    payload = "" # need to build payload
+def build_light_set_power_packet(source, level_value, duration_value):
+    field_dictionary = {"tagged": 0, "source": source, "target": 0, "ack": 0, "res": 0, "sequence": 0, "type": 117}
+    level = int_to_bin(level_value, 16)#unsigned 16 bit integer (0 or 65535)
+    duration = int_to_bin(duration_value, 32)#unsigned 32 bit integer (transition in milliseconds)
+    payload = level + duration
     return PacketBuilderBase.buildPacket(field_dictionary, payload)
 
-def build_light_set_waveform_optional_packet():
+def build_light_set_waveform_optional_packet(source, transient_value, hue_value, sat_value, brightness_value, kelvin_value, period_value, cycles_value, skew_ration_value, waveform_value, set_hue_value, set_sat_value, set_brightness_value, set_kelvin_value):
     field_dictionary = {"tagged": 0, "source": 0, "target": 0, "ack": 0, "res": 0, "sequence": 0, "type": 119}
-    payload = "" # need to build payload
+    reserved = "".zfill(8)#unsigned 8 bit integer
+    transient = transient_value#8 bit integer (0 or 1)
+    hue = int_to_bin(hue_value, 16)#unsigned 16 bit integer (0-65535)
+    saturation = int_to_bin(sat_value, 16)#unsigned 16 bit integer (0-65535)
+    brightness = int_to_bin(brightness_value, 16)#unsigned 16 bit integer (0-65535)
+    kelvin = int_to_bin(kelvin_value, 16)#unsigned 16 bit integer (2500-9000)
+    period = period_value#unsigned 32 bit integer (cycle duration in milliseconds)
+    cycles = cycles_value#32 bit float (number of cycles)
+    skew_ratio = skew_ration_value#16 bit integer
+    waveform = waveform_value#unsinged 8 bit integer
+    set_hue = set_hue_value#8 bit integer (0 or 1)
+    set_saturation = set_sat_value#8 bit integer (0 or 1)
+    set_brightness = set_brightness_value#8 bit integer (0 or 1)
+    set_kelvin = set_kelvin_value#8 bit integer (0 or 1)
+    payload = reserved + transient + hue + saturation + brightness + kelvin + period + cycles + skew_ratio + waveform + set_hue + set_saturation + set_brightness + set_kelvin
     return PacketBuilderBase.buildPacket(field_dictionary, payload)
 
 def build_light_get_infrared_packet():
@@ -119,6 +156,6 @@ def build_light_get_infrared_packet():
 
 def build_light_set_infrared_packet(brightness_value):
     field_dictionary = {"tagged": 0, "source": 0, "target": 0, "ack": 0, "res": 0, "sequence": 0, "type": 122}
-    brightness = brightness_value
-    payload = "" # need to build payload
+    brightness = int_to_bin(brightness_value, 16)#unsigned 16 bit integer
+    payload = brightness
     return PacketBuilderBase.buildPacket(field_dictionary, payload)
