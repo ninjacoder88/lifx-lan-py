dcb3fc61834f43a4720eca5b9809e33ce4c1af66
ninjacoder88 <email@harrisconsulting.xyz>
2018-12-21 12:03:08 -0600

    changed method being called

--- src/ProgramBroadcast.py
+++ src/ProgramBroadcast.py
@@ -3,7 +3,7 @@ import PacketBuilder
 
 def broadcast():
     
-    packet_bytes = PacketBuilder.build_device_get_service_packet()
+    packet_bytes = PacketBuilder.build_light_get_state_packet()
     
     PacketBroadcaster.broadcast_packet("172.16.10.255", 56700, packet_bytes)
     return
