9eb6165a024d7c3cd183ff0e5e7cd390611c6246
ninjacoder88 <email@harrisconsulting.xyz>
2019-02-23 21:20:59 -0600

    added file to parse command options and call PacketBuilder

---
+++ src/CommandHandler.py
@@ -0,0 +1,142 @@
+import PacketBuilder
+
+def handle_help():
+    return ""
+
+def handle_device_get_service(options):
+    available_options = {
+        "-help": "provides help document",
+        "-a": "all devices"
+    }
+    print("not yet implemented")
+    return ""
+    
+def handle_device_get_host_info(command):
+    available_options = {
+        "-help": "provides help document",
+        "-a": "all devices"
+    }
+    print("not yet implemented")
+    return ""
+
+def handle_device_get_host_firmware(command):
+    available_options = {
+        "-help": "provides help document",
+        "-a": "all devices"
+    }
+    print("not yet implemented")
+    return ""
+
+def handle_device_get_wifi_info(command):
+    available_options = {
+        "-help": "provides help document",
+        "-a": "all devices"
+    }
+    print("not yet implemented")
+    return ""
+
+def handle_device_get_wifi_firmware(command):
+    available_options = {
+        "-help": "provides help document",
+        "-a": "all devices"
+    }
+    print("not yet implemented")
+    return ""
+
+def handle_device_get_power(command):
+    available_options = {
+        "-help": "provides help document",
+        "-a": "all devices"
+    }
+    print("not yet implemented")
+    return ""
+
+def handle_device_set_power(command):
+    available_options = {
+        "-help": "provides help document",
+        "-a": "all devices",
+        "-t:Target": "target device mac address",
+        "-l:Level": "power level, must be 0 or 1"
+    }
+    print("not yet implemented")
+    return ""
+
+def handle_device_get_label(command):
+    available_options = {}
+    print("not yet implemented")
+    return ""
+
+def handle_device_set_label(command):
+    available_options = {}
+    print("not yet implemented")
+    return ""
+
+def handle_device_get_version(command):
+    available_options = {}
+    print("not yet implemented")
+    return ""
+
+def handle_device_get_info(command):
+    available_options = {}
+    print("not yet implemented")
+    return ""
+
+def handle_device_set_location(command):
+    available_options = {}
+    print("not yet implemented")
+    return ""
+
+def handle_device_get_group(command):
+    available_options = {}
+    print("not yet implemented")
+    return ""
+
+def handle_device_set_group(command):
+    available_options = {}
+    print("not yet implemented")
+    return ""
+
+def handle_device_echo_request(command):
+    available_options = {}
+    print("not yet implemented")
+    return ""
+
+def handle_light_get_state(command):
+    available_options = {}
+    print("not yet implemented")
+    return ""
+
+def handle_light_set_color(command):
+    available_options = {}
+    print("not yet implemented")
+    return ""
+
+def handle_light_set_waveform(command):
+    available_options = {}
+    print("not yet implemented")
+    return ""
+
+def handle_light_get_power(command):
+    available_options = {}
+    print("not yet implemented")
+    return ""
+
+def handle_light_set_power(command):
+    available_options = {}
+    print("not yet implemented")
+    return ""
+
+def handle_light_set_waveform_optional(command):
+    available_options = {}
+    print("not yet implemented")
+    return ""
+
+def handle_light_get_infrared(command):
+    available_options = {}
+    print("not yet implemented")
+    return ""
+
+def handle_light_set_infrared(command):
+    available_options = {}
+    print("not yet implemented")
+    return ""
